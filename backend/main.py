a7efd80eac2d651326c9a1701747ca3c6f2bef93
SebastVR <sebastianvr92@hotmail.com>
2024-04-09 11:43:50 -0500

    Expor library rigth #1

--- backend/main.py
+++ backend/main.py
@@ -1,25 +1,8 @@
-# from typing import Union
-
-# from fastapi import FastAPI
-
-# app = FastAPI()
-
-
-# @app.get("/")
-# def read_root():
-#     return {"Hello": "World"}
-
-
-# @app.get("/items/{item_id}")
-# def read_item(item_id: int, q: Union[str, None] = None):
-#     return {"item_id": item_id, "q": q}
-
-
 from fastapi import FastAPI
 
-# from router import item_router
-# from router.router_items import item_router
-from router import item_router, file_read_router
+
+from router.file_read import file_router
+from router.router_items import item_router
 
 app = FastAPI()
 
@@ -31,7 +14,7 @@ app.include_router(
 
 
 app.include_router(
-    file_read_router,
+    file_router,
     prefix="/files",  # Define el prefijo para todas las rutas definidas en el router.
     tags=["files"],  # Agrega etiquetas para organizar tus rutas.
 )
