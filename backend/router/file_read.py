17676c88dae0730bf90f5c3b9eaacfd749deb273
SebastVR <sebastianvr92@hotmail.com>
2024-04-09 15:34:08 -0500

    Add api get of api read_file #1

--- backend/router/file_read.py
+++ backend/router/file_read.py
@@ -2,6 +2,8 @@ from fastapi import APIRouter, File as FastAPIFile, UploadFile, Depends
 from sqlalchemy.orm import Session
 from core.dependencies import get_db
 from controller.file_read import read_pdf_and_save
+from models.file_read import File
+
 
 file_router = APIRouter()
 
@@ -10,4 +12,10 @@ file_router = APIRouter()
 async def read_pdf(file: UploadFile = FastAPIFile(...), db: Session = Depends(get_db)):
     file_data = await file.read()
     pdf_contents = read_pdf_and_save(file_data, file.filename, db)
-    return {"pdf_contents": pdf_contents}
+    return {"values": pdf_contents}
+
+
+@file_router.get("/pdf")
+async def read_pdf_get(db: Session = Depends(get_db)):
+    files = db.query(File).all()
+    return {"values": [file.title for file in files]}
