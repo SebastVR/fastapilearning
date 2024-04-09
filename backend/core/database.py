0af55a5992bf4c4c6d50855df1d43df565d8d168
SebastVR <sebastianvr92@hotmail.com>
2024-04-09 11:34:05 -0500

    Create folder core for put file settings #1

--- backend/core/database.py
+++ backend/core/database.py
@@ -1,19 +1,14 @@
-# database.py
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # Asegúrate de ajustar la importación según tu estructura de proyecto
-from .settings import settings
+from core.settings import settings
 
+# Usa la propiedad DATABASE_URL de la instancia settings para la conexión
+SQLALCHEMY_DATABASE_URL = settings.DATABASE_URL
 
-# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
-SQLALCHEMY_DATABASE_URL = settings.database_url
-
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL,
-    pool_pre_ping=True,
-)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
