345b4205c536d783dc1349edf73aa63e71b69cdd
SebastVR <sebastianvr92@hotmail.com>
2024-05-15 16:28:45 -0500

    Adjusment controllre #1

--- backend/controller/detection_ppe.py
+++ backend/controller/detection_ppe.py
@@ -7,13 +7,22 @@ from models.detection_ppe import Project, Detection  # Verifica la ruta de impor
 from ultralytics import (
     YOLO,
 )  # Asegúrate de tener esta biblioteca correctamente instalada y configurada
-
+import logging
 from datalake.save_s3 import SaveS3
 from datetime import datetime, timedelta
 
+logging.basicConfig(
+    level=logging.DEBUG, format="%(asctime)s - %(levelname)s - %(message)s"
+)
 # Configuración del directorio de imágenes
+# IMAGE_DIR = Path("data/media")
+# IMAGE_DIR.mkdir(parents=True, exist_ok=True)
+# MODEL_PATH = Path("data/staticfiles/best.pt")
+
 IMAGE_DIR = Path("data/media")
 IMAGE_DIR.mkdir(parents=True, exist_ok=True)
+PROCESSED_DIR = Path("data/processed")
+PROCESSED_DIR.mkdir(parents=True, exist_ok=True)
 MODEL_PATH = Path("data/staticfiles/best.pt")
 
 # Lista de todos los posibles elementos de EPP
@@ -33,41 +42,57 @@ EPP_ITEMS = [
 
 
 def process_image(file: UploadFile, db: Session, project_id: int) -> Detection:
-    """
-    Procesa una imagen cargada utilizando YOLO para detectar elementos de protección personal,
-    carga la imagen a MinIO y registra los detalles de la detección en la base de datos.
-    """
-    # Instanciar SaveS3 dentro de la función para utilizarlo para subir el archivo
     s3_saver = SaveS3()
-
-    # Asegúrate de que el directorio existe
-    IMAGE_DIR.mkdir(parents=True, exist_ok=True)
-
-    # Guardar la imagen localmente si es necesario (opcional)
     local_image_path = save_image(file)
 
-    # Cargar la imagen a MinIO y obtener la ruta en el datalake
-    bucket_name = "project-ppe-detection-datalake"  # Cambiar por el nombre real de tu bucket en MinIO
-    object_name = f"images/{file.filename}"
-    datalake_image_path = s3_saver.write_image_to_minio(bucket_name, object_name, file)
+    # Cargar la imagen original a MinIO
+    bucket_name = "project-ppe-detection-datalake"
+    object_name_original = f"original/{file.filename}"
+    with open(local_image_path, "rb") as img_file:
+        datalake_image_path_original = s3_saver.write_image_to_minio(
+            bucket_name, object_name_original, img_file.read()
+        )
+
     # Procesar la imagen con YOLO
+    datalake_image_path_proceada = PROCESSED_DIR / "procesada"
     model = YOLO(MODEL_PATH.as_posix())
-    results = model.predict([local_image_path.as_posix()])
+    results = model.predict(
+        [local_image_path.as_posix()],
+        save=True,
+        project=datalake_image_path_proceada.as_posix(),
+    )
+
+    # Revisar qué se ha guardado realmente en el directorio
+    processed_files = list(datalake_image_path_proceada.glob("**/*"))
+    if processed_files:
+        for processed_file in processed_files:
+            if (
+                processed_file.is_file()
+            ):  # Verificar si es un archivo y no un directorio
+                with open(processed_file, "rb") as processed_img_file:
+                    processed_image_path = s3_saver.write_image_to_minio(
+                        bucket_name,
+                        f"procesada/{processed_file.name}",
+                        processed_img_file.read(),
+                    )
+                break
+    else:
+        processed_image_path = "No processed image found"
+
+    # Procesar los resultados
     json_data = results[0].tojson()
     detections = json.loads(json_data)
-
-    # Contabilizar los elementos detectados
     detection_counts = {item: 0 for item in EPP_ITEMS}
     for detection in detections:
         if detection["name"] in EPP_ITEMS:
             detection_counts[detection["name"]] += 1
 
-    # Crear un nuevo registro de detección en la base de datos
-    current_datetime = datetime.utcnow()
+    # Registro en la base de datos
     new_detection = Detection(
-        datalake_image_path=datalake_image_path,
+        datalake_image_path=datalake_image_path_original,
+        datalake_image_processed=processed_image_path,  # Usar la ruta procesada
         project_id=project_id,
-        created_at=current_datetime,
+        created_at=datetime.utcnow(),
         **detection_counts,
     )
     db.add(new_detection)
@@ -78,9 +103,6 @@ def process_image(file: UploadFile, db: Session, project_id: int) -> Detection:
 
 
 def save_image(file: UploadFile) -> Path:
-    """
-    Guarda un archivo cargado localmente y devuelve la ruta al archivo guardado.
-    """
     file_location = IMAGE_DIR / file.filename
     with file_location.open("wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
@@ -100,3 +122,17 @@ def create_project(
 def get_project(db: Session, project_id: int) -> Project:
     project = db.query(Project).filter(Project.id == project_id).first()
     return project
+
+
+def delete_project(db: Session, project_id: int) -> None:
+    project = db.query(Project).filter(Project.id == project_id).first()
+    if project:
+        db.delete(project)
+        db.commit()
+
+
+def delete_detection(db: Session, detection_id: int) -> None:
+    detection = db.query(Detection).filter(Detection.id == detection_id).first()
+    if detection:
+        db.delete(detection)
+        db.commit()
